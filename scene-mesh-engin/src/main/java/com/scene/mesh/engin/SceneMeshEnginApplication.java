622eab93d757d0b848677f4e36407fe2ee82e54a
严先生 <yanfang280@gmail.com>
2025-08-12 14:55:47 +0800

    Feature (#12)
    * merger test8
    
    * 111 (#5)
    
    * Feature (#2)
    
    * merger test6
    
    * merger test7
    
    ---------
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    * merger test8 (#3)
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    * merger test8 (#4)
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    ---------
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    * merger (#7)
    
    * Feature (#2)
    
    * merger test6
    
    * merger test7
    
    ---------
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    * merger test8 (#3)
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    * merger test8 (#4)
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    ---------
    
    Co-authored-by: yanfang <yanf820@163.com>
    
    * merger 10
    
    * merger test11
    
    ---------
    
    Co-authored-by: yanfang <yanf820@163.com>

--- scene-mesh-engin/src/main/java/com/scene/mesh/engin/SceneMeshEnginApplication.java
+++ scene-mesh-engin/src/main/java/com/scene/mesh/engin/SceneMeshEnginApplication.java
@@ -75,7 +75,7 @@ public class SceneMeshEnginApplication {
         //startup
         try {
             processManager.executeProcesses(graphId,args);
-            System.out.println("merger test10");
+            System.out.println("merger test11");
 //            processManager.executeAllProcesses();
         } catch (Exception e) {
             e.printStackTrace();
